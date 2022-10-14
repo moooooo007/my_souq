f899f8423c2ded54e5d54381da371f31d752b7a3
Mohammed Safaa <ictp.m2010@gmail.com>
2022-10-14 14:54:35 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/my_souq/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_souq
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+main() {
+
+  runApp (const MySouq());
+}
+
+class MySouq extends StatelessWidget {
+  const MySouq({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+
+      },
+      initialRoute: ,
+    );
+  }
+}
